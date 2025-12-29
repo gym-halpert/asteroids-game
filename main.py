5513315c156e69f084cfac11deed0f4b7c107d3f
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2025-12-28 19:22:38 -0700

    Added asteroids at random

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from circleshape import CircleShape
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
@@ -16,10 +18,14 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     print("Starting Asteroids with pygame version: 2.6.1")
     print("Screen width: 1280")
