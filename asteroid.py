645b508cd6da6d29fab85533a5542b019fba3cb9
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2025-12-30 21:13:25 -0700

    Added split method to asteroids upon shot contact

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from constants import *
 from circleshape import CircleShape
+from logger import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,4 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        vector1 = self.velocity.rotate(angle)
+        vector2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = vector1 * 1.2
+        asteroid2.velocity = vector2 * 1.2
 
